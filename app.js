dc09ff3ddcdbd10b4bde4f0014a0c0932238c780
kosuke.usui <51241343+kskwes@users.noreply.github.com>
2026-05-11 10:57:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // =============================================
 const GEMINI_API_KEY = 'AIzaSyAhqfU5Sw2EFKURNW4lAyLxLd7xF5AGGcA';
 
-const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 const CACHE_KEY = 'koyomi_kigo_cache';
 
 // =============================================
